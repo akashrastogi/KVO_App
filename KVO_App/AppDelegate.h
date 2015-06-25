7b356d73354396f7c45ede15bfe493a0844df14b
Akash Rastogi <ios@quippp.com>
2015-06-25 22:47:33 +0530

    Initial Commit

---
+++ KVO_App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVO_App
+//
+//  Created by Akash Rastogi on 25/06/15.
+//  Copyright (c) 2015 Akash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVO_App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KVO_App
+//
+//  Created by Akash Rastogi on 25/06/15.
+//  Copyright (c) 2015 Akash. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
